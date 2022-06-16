8ba8197406b0d711ba5b249c560dda5188df84fb
Kestutis <kestutis.julius@gmail.com>
2022-06-16 13:25:04 +0300

    jSon changes

--- seeder.php
+++ seeder.php
@@ -2,12 +2,11 @@
 require __DIR__.'./db/JsonDb.php';
 
 $animals = ['Sheep', 'Horse', 'Duck', 'Pig', 'Goat', 'Small Donkey', 'Duc', 'Cow'];
-
+$db = new JsonDb('farm');
 $seed = [];
 foreach(range(1,11) as $_){
-   $seed[] = ['animal' => $animals[rand(0, count($animals)-1)], 'weight' => rand(100, 20000)/100]; 
+   $db->create(['animal' => $animals[rand(0, count($animals)-1)], 'weight' => rand(100, 20000)/100]); 
     // $seed[] = 'animal'=>$animals[rand(1, count($animals))], 'weight' => rand(100, 20000)/100;
 }
-$db = new JsonDb('farm');
-$db->create($seed);
+
 
